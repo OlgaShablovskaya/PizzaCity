50d0bac3e7b2f8fa1b2cbd086875f29144c25b92
OlgaShablovskaya <151164669+OlgaShablovskaya@users.noreply.github.com>
2024-03-04 17:05:29 +0300

    Update PizzaMoscow.kt

--- src/PizzaMoscow.kt
+++ src/PizzaMoscow.kt
@@ -8,25 +8,31 @@ class PizzaMoscow (
     override fun showCheckPhoto() {
         println("У вас есть фотография чека?")
         println("1. Да\n 2. Нет")
-        if (readln()=="1") println("Вам будет скидка 50 рублей с покупки")
+        val choice = readln()
+        if (choice == "1") {
+            val discount = 50.0
+            println("Вам будет скидка $discount рублей с покупки")
+            totalDiscounts += discount //Общая сумма скидок(*в руб*)
+            totalCheckPhotos++ // количество показанных чеков
+        }
     }
     override fun neapolitanPizzaSale() {
-        neapolitanPizzaCount++
+        neapolitanPizzaCount++ // подсчет для пицц
         println("Спасибо за покупку неаполитанской пиццы в Москве")
     }
 
     override fun romanPizzaSale() {
-        romanPizzaCount++
+        romanPizzaCount++ // подсчет для пицц
         println("Спасибо за покупку  римской пиццы в Москве")
     }
 
     override fun sicilianPizzaSale() {
-        sicilianPizzaCount++
+        sicilianPizzaCount++ // подсчет для пицц
         println("Спасибо за покупку сицилийской пиццы в Москве")
     }
 
     override fun tyroleanPizzaSale() {
-        tyroleanPizzaCount++
+        tyroleanPizzaCount++ // подсчет для пицц
         println("Спасибо за покупку тирольской пиццы в Москве")
     }
 }
